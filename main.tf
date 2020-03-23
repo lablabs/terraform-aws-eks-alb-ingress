cceea956c67c1a4170af0eac09e28b058eb77861
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2020-03-23 20:41:00 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "helm_repository" "default" {
+  name = var.helm_repo_name
+  url  = var.helm_repo_url
+}
+
+resource "helm_release" "alb_ingress" {
+  count      = var.enabled ? 1 : 0
+  name       = var.helm_release_name
+  repository = data.helm_repository.default.metadata[0].name
+  chart      = var.helm_chart_name
+  namespace  = var.k8s_namespace
+  version    = var.helm_chart_version
+
+  values = [
+    "${templatefile("${path.module}/templates/values.yaml.tpl",
+      {
+        "cluster_name"             = var.cluster_name,
+        "alb_ingress_iam_role_arn" = aws_iam_role.alb_ingress[0].arn
+        "replica_count"            = var.replica_count
+        "ingress_class"            = var.ingress_class
+      })
+    }"
+  ]
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+# Required module inputs
+variable "cluster_name" {}
+variable "cluster_identity_oidc_issuer" {}
+variable "cluster_identity_oidc_issuer_arn" {}
+
+# alb-ingress
+variable "enabled" {
+  type = bool
+}
+
+variable "replica_count" {
+  default = 3
+}
+
+variable "ingress_class" {
+  default = "alb-ingress"
+}
+
+# Helm
+variable "helm_chart_name" {
+  default = "aws-alb-ingress-controller"
+}
+
+variable "helm_chart_version" {
+  default = "0.1.13"
+}
+
+variable "helm_release_name" {
+  default = "aws-alb-ingress-controller"
+}
+
+variable "helm_repo_name" {
+  default = "incubator"
+}
+
+variable "helm_repo_url" {
+  default = "http://storage.googleapis.com/kubernetes-charts-incubator"
+}
+
+# K8S
+variable "k8s_namespace" {
+  default     = "alb-ingress"
+  description = "The k8s namespace in which the alb-ingress service account has been created"
+}
+
+variable "k8s_service_account_name" {
+  default     = "aws-alb-ingress-controller"
+  description = "The k8s alb-ingress service account name"
+}
