2b637e77e73f693b974f81b2a6098b63b75279ea
Stephen Hoekstra <shoekstra@schubergphilis.com>
2021-03-08 17:16:09 +0100

    Use the iam_policy.json from the install docs
    To keep the policy up to date with the recommended settings we now pull
    this directly from th eample. If we find we need to customise it in
    the future, we'll duplicate it in the module.

--- iam.tf
+++ iam.tf
@@ -9,137 +9,11 @@ resource "kubernetes_namespace" "alb_ingress" {
 
 ### iam ###
 # Policy
-data "aws_iam_policy_document" "alb_ingress" {
+data "http" "alb_ingress_policy" {
   depends_on = [var.mod_dependency]
   count      = var.enabled ? 1 : 0
 
-  statement {
-    actions = [
-      "acm:DescribeCertificate",
-      "acm:ListCertificates",
-      "acm:GetCertificate"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CreateSecurityGroup",
-      "ec2:CreateTags",
-      "ec2:DeleteTags",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DescribeAccountAttributes",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeInstances",
-      "ec2:DescribeInstanceStatus",
-      "ec2:DescribeInternetGateways",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeTags",
-      "ec2:DescribeVpcs",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:ModifyNetworkInterfaceAttribute",
-      "ec2:RevokeSecurityGroupIngress"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "elasticloadbalancing:AddListenerCertificates",
-      "elasticloadbalancing:AddTags",
-      "elasticloadbalancing:CreateListener",
-      "elasticloadbalancing:CreateLoadBalancer",
-      "elasticloadbalancing:CreateRule",
-      "elasticloadbalancing:CreateTargetGroup",
-      "elasticloadbalancing:DeleteListener",
-      "elasticloadbalancing:DeleteLoadBalancer",
-      "elasticloadbalancing:DeleteRule",
-      "elasticloadbalancing:DeleteTargetGroup",
-      "elasticloadbalancing:DeregisterTargets",
-      "elasticloadbalancing:DescribeListenerCertificates",
-      "elasticloadbalancing:DescribeListeners",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticloadbalancing:DescribeLoadBalancerAttributes",
-      "elasticloadbalancing:DescribeRules",
-      "elasticloadbalancing:DescribeSSLPolicies",
-      "elasticloadbalancing:DescribeTags",
-      "elasticloadbalancing:DescribeTargetGroups",
-      "elasticloadbalancing:DescribeTargetGroupAttributes",
-      "elasticloadbalancing:DescribeTargetHealth",
-      "elasticloadbalancing:ModifyListener",
-      "elasticloadbalancing:ModifyLoadBalancerAttributes",
-      "elasticloadbalancing:ModifyRule",
-      "elasticloadbalancing:ModifyTargetGroup",
-      "elasticloadbalancing:ModifyTargetGroupAttributes",
-      "elasticloadbalancing:RegisterTargets",
-      "elasticloadbalancing:RemoveListenerCertificates",
-      "elasticloadbalancing:RemoveTags",
-      "elasticloadbalancing:SetIpAddressType",
-      "elasticloadbalancing:SetSecurityGroups",
-      "elasticloadbalancing:SetSubnets",
-      "elasticloadbalancing:SetWebACL"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "iam:CreateServiceLinkedRole",
-      "iam:GetServerCertificate",
-      "iam:ListServerCertificates"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "waf-regional:GetWebACLForResource",
-      "waf-regional:GetWebACL",
-      "waf-regional:AssociateWebACL",
-      "waf-regional:DisassociateWebACL"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "tag:GetResources",
-      "tag:TagResources"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
-  statement {
-    actions = [
-      "waf:GetWebACL"
-    ]
-    resources = [
-      "*",
-    ]
-    effect = "Allow"
-  }
-
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
 }
 
 resource "aws_iam_policy" "alb_ingress" {
@@ -149,7 +23,7 @@ resource "aws_iam_policy" "alb_ingress" {
   path        = "/"
   description = "Policy for alb-ingress service"
 
-  policy = data.aws_iam_policy_document.alb_ingress[0].json
+  policy = data.http.alb_ingress_policy[0].body
 }
 
 # Role
